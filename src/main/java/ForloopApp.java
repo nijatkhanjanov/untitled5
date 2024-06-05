50d6fec56ac4e85ed2c06601a40b608336b25bb3
Nijat <nijatkhanjnov@glmail.com>
2024-06-05 17:08:36 +0400

    committed

---
+++ src/main/java/ForloopApp.java
@@ -0,0 +1,10 @@
+public class ForloopApp {
+    public static void main(String[] args) {
+        for (int i = 7; i >0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    }
